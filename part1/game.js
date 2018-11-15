6e14c70d7c20838c78471403f4f33c18f23cfd0a
jeffchen0124 <44307055+jeffchen0124@users.noreply.github.com>
2018-11-15 14:55:24 -0800

    Update game.js

--- part1/game.js
+++ part1/game.js
@@ -192,7 +192,7 @@ Game = {
 
     initialize: function(id, game, cfg) {
       this.cfg          = Object.extend(game.Defaults || {}, cfg || {}); // use game defaults (if any) and extend with custom cfg (if any)
-      this.fps          = this.cfg.fps || 60;
+      this.fps          = 144;
       this.interval     = 1000.0 / this.fps;
       this.canvas       = document.getElementById(id);
       this.width        = this.cfg.width  || this.canvas.offsetWidth;
